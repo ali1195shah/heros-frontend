4bdede024a566ae3a7bc1198a2bc7d2aba7d1055
Ali <ali1195shah@gmail.com>
2020-01-06 21:57:23 -0500

    Doing CRUD later

--- src/components/DetailCharacterPage.js
+++ src/components/DetailCharacterPage.js
@@ -15,15 +15,22 @@ export class DetailCharacterPage extends Component {
             <li>Real Name: { this.props.character.full_name }</li>
             <li>Aliases: { this.props.character.aliases}</li>
             <li>Birth place: { this.props.character.place_of_birth }</li>
-            <li>Gender: {this.props.character.gender}</li>
-            <li>Race: {this.props.character.race}</li>
-            <li>Eye color: {this.props.character.eye_color}</li>
-            <li>Hair color: {this.props.character.hair_color}</li>
-            <li>Height: {this.props.character.height_feet}</li>
+            <li>Gender: { this.props.character.gender }</li>
+            <li>Race: { this.props.character.race }</li>
+            <li>Eye color: { this.props.character.eye_color }</li>
+            <li>Hair color: { this.props.character.hair_color }</li>
+            <li>Intelligence: { this.props.character.intelligence }</li>
+            <li>Strength: { this.props.character.strength }</li>
+            <li>Speed: { this.props.character.speed }</li>
+            <li>Durability: { this.props.character.durability }</li>
+            <li>Power: { this.props.character.power }</li>
+            <li>Combat: { this.props.character.combat }</li>
+            <li>Height: { this.props.character.height_feet}</li>
             <li>Relatives: { this.props.character.relatives }</li>
             <li>Occupation: { this.props.character.occupation }</li>
             <li>Group Affiliation: { this.props.character.group_affiliation }</li>
-    </ul>
+            
+        </ul>
     </center>
     
 </div>
@@ -33,7 +40,7 @@ export class DetailCharacterPage extends Component {
                 </div>    
         }
         <div>
-                            <button onClick={ this.props.goBack } >Go Back</button>
+                            <center><button onClick={ this.props.goBack } >Go Back</button></center>
                         </div>
         </div>
         );
@@ -41,3 +48,7 @@ export class DetailCharacterPage extends Component {
 }
 
 export default DetailCharacterPage;
+
+
+
+
